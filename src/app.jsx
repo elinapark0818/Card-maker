16a333a75eab9d77711bf49e9fb7f51831142f47
Elina <elinapark0818@gmail.com>
2021-08-02 21:50:19 +0900

    reset

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,13 @@
-import styles from './app.module.css';
-import Login from './components/login/login';
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router';
+import './app.css';
+import React from 'react';
 
-import { auth } from './firebase.utills';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
+const App = (props) => 
 
-  componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-    })
-  }
+  <>
+  <h1>HELLO </h1>
+  </>
 
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={Login} />
-        </Switch>
-      </div>
-    );
-  }
-}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.module.css';
 import App from './app';
 
-import { BrowserRouter } from 'react-router-dom';
-import firebase from './firebase.utills';
 
-console.log(firebase);
 
 ReactDOM.render(
-  <BrowserRouter>
+  <>
     <App />
-  </BrowserRouter>,
+  </>,
   document.getElementById('root')
 );

715e50e60cbe892709c0d793cab7083a23c27679
Elina <elinapark0818@gmail.com>
2021-08-02 21:11:02 +0900

    firebase setup

--- src/firebase.utills.js
+++ src/firebase.utills.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();

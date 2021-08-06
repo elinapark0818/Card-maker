95fedef4b02f86d868436cb407915dc1e5daa722
Elina <elinapark0818@gmail.com>
2021-08-06 23:01:29 +0900

    add editor, preview

---
+++ src/components/editor/editor.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './editor.module.css';
+
+const Editor = props => 
+<section className={styles.editor}>
+  <h1>Editor</h1>
+</section>
+
+export default Editor;
